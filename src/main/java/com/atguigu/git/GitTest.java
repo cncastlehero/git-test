4d7825f8865ccb45358566927ddb480bcf7d9e22
layne <layne@guigu.com>
2022-11-24 21:10:55 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,9 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git~!");
         System.out.println("hello git~2!");
+        System.out.println("hello git~4!");
+        System.out.println("master text!");
+
 
     }
 }
